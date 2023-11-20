0c94bbc1d4c038817e28298a3cfd2d468153556a
Brandon Brockett <bbrockett903@gmail.com>
2023-11-20 04:53:46 +0000

    assignment commit 2

--- lab3.c
+++ lab3.c
@@ -51,69 +51,63 @@ int is_board_valid(){
     return true;
 }
 
-bool isValidColumn(char board[SIZE][SIZE], int col) {
-    bool visited[SIZE + 1] = { false };
+int is_board_valid() {
+    worker_validation = (int*) malloc(sizeof(int)*NUM_OF_THREADS);
+    pthread_t *tid = (pthread_t*) malloc(sizeof(int)*NUM_OF_THREADS);
+    pthread_attr_t attr;
+    param_struct* params = (param_struct*)malloc(sizeof(param_struct)*NUM_OF_THREADS);
+    for(int i= 0;i<ROW_SIZE;i++) {
+        params[i].starting_row = i;
+        params[i].starting_col = 0;
+        params[i].ending_row = i;
+        params[i].ending_col = COL_SIZE-1;
+        pthread_create(&(tid[i]), &attr, validate_row(sudoku_board,params[i]), &(params[i]));
 
-    for (int i = 0; i < SIZE; ++i) {
-        int num = board[i][col] - '0';
-        if (num < 1 || num > 9 || visited[num])
-            return false;
-        visited[num] = true;
     }
 
-    return true;
-}
 
-bool isValidSubgrid(char board[SIZE][SIZE], int startRow, int startCol) {
-    bool visited[SIZE + 1] = { false };
-
-    for (int i = startRow; i < startRow + 3; ++i) {
-        for (int j = startCol; j < startCol + 3; ++j) {
-            int num = board[i][j] - '0';
-            if (num < 1 || num > 9 || visited[num])
-                return false;
-            visited[num] = true;
+    for(int i= ROW_SIZE;i<ROW_SIZE+COL_SIZE;i++) {
+        params[i].starting_row = 0;
+        params[i].starting_col = i;
+        params[i].ending_row = ROW_SIZE-1;
+        params[i].ending_col = i;
+        pthread_create(&(tid[i]), &attr, validate_col(), &(params[i]));
+    }
+    for(int i =0;i<NUM_OF_THREADS;i++) {
+        if (worker_validation[i]!=1) {
+            return 0;
         }
     }
+    return 1;
 
-    return true;
+    
+
 }
 
-bool isValidSudoku(char board[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; ++i) {
-        if (!isValidRow(board, i) || !isValidColumn(board, i))
-            return false;
-    }
+    
 
-    for (int i = 0; i < SIZE; i += 3) {
-        for (int j = 0; j < SIZE; j += 3) {
-            if (!isValidSubgrid(board, i, j))
-                return false;
+
+ int validate_row(int** board, param_struct params) {
+    int validate_arr[9];
+    for(int i =params.starting_row; i<params.ending_row;i++) {
+        for (int j = params.starting_col;i<params.ending_row;i++) {
+            int current_row_val = board[i][j];
+            validate_arr[current_row_val-1] = current_row_val - (current_row_val-1);
+
         }
     }
+    for(int i = 0;i<9;i++) {
+        if(validate_arr[i]!=1) {
+            return 0;
 
-    return true;
+        }
+    }
+    return 1;
+ }
+int validate_col() {
+    
 }
 
-int main() {
-    char board[SIZE][SIZE] = {
-        {'5', '3', '4', '6', '7', '8', '9', '1', '2'},
-        {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
-        {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
-        {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
-        {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
-        {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
-        {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
-        {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
-        {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
-    };
-
-    if (isValidSudoku(board))
-        printf("Valid Sudoku board!\n");
-    else
-        printf("Invalid Sudoku board!\n");
-
-    return 0;
-}
-
+int validate_subgrid() {
+    return 1;
 }
