2ee350f8c8102d8e3ed1d416c608f544aa66ce8d
Brandon Brockett <bbrockett903@gmail.com>
2023-12-02 04:00:51 +0000

    assignment commit 4

--- lab3.c
+++ lab3.c
@@ -8,26 +8,113 @@ extern int** sudoku_board;
 int* worker_validation;
 
 int** read_board_from_file(char* filename){
+    FILE *fp = fopen(filename, "r");
+    sudoku_board = (int**)malloc(9*sizeof(int*));
+    for (int col = 0; col < 9; col++)
+    {
+        sudoku_board[col] = (int*)malloc(9*sizeof(int));
+    }
+    if(fp == NULL){
+        fprintf(stderr, "No file.");
+        return NULL;
+        }
+void* board_piece(void* piece){
+    param_struct* tester = (param_struct*) piece;
+    int spot = tester->id;
+    int sr = tester->starting_row;
+    int er = tester->ending_row;
+    int sc = tester->starting_col;
+    int ec = tester->ending_col;
+    
+    valid[spot] = 1;
+    
 
-    FILE *fp = fopen(filename,"r");
-    int** board = (int**)malloc(ROW_SIZE * sizeof(int*));
+    int checker[9];
+    for (int row = sr; row <= er; row++){
+        for (int col = sc; col <= ec; col++){
+            checker[ sudoku_board[row][col] -1] = 1;
+        }
+    } 
 
-    for(int i = 0; i < COL_SIZE; i++)
-        board[i] = (int*) malloc(COL_SIZE * sizeof(int));
-
-    if(fp == NULL) return;
-
-
-    for(int r = 0; r < ROW_SIZE; r++)
-        for(int c = 0; c < COL_SIZE; c++)
-            fscanf(fp,"%d",&board[r][c]);
+    for (int value =0; value < 9; value++){
+        if(checker[value] != 1){
+            valid[spot] = 0;
+        }
+    }
+}
+    int row = 0;
+    while(row < 9){
+        int col = 0;
+        while(col < 9){
+            fscanf(fp,"%d%*c",&sudoku_board[row][col]);
+            col++;
+        }
+        row++;
+    }
     fclose(fp);
+    return sudoku_board;
+}
+int is_board_valid(){
+    pthread_t* tid;  /* the thread identifiers */
+    pthread_attr_t attr;
+    param_struct* parameter;
+    tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);   
+    parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
+    valid = (int*)malloc(27*sizeof(int));
+    int identity = 0;
+    for (int spot = 0; spot < ROW_SIZE; spot++){
+        parameter[identity].id = identity;
+        parameter[identity].starting_row = spot;
+        parameter[identity].starting_col = 0;
+        parameter[identity].ending_col = COL_SIZE-1;
+        parameter[identity].ending_row = spot;
+        pthread_create(&(tid[identity]), NULL, board_piece, &parameter[identity]);
+        identity++;
+    }
 
-    return board;
+    for (int spot = 0; spot < COL_SIZE; spot++){
+        param[identity].id = identity;
+        param[identity].starting_row = 0;
+        param[identity].starting_col = spot;
+        param[identity].ending_col = spot;
+        param[identity].ending_row = ROW_SIZE-1;
+        pthread_create(&(tid[identity]), NULL, board_piece, &param[identity]);
+        identity++;
+    }
+    
+    int horz = 0;
+    int vert = 0;
+    for (int spot = 0; spot < NUM_OF_SUBGRIDS; spot++){
+        param[identity].id = identity;
+        param[identity].starting_row = horz;
+        param[identity].starting_col = vert;
+        param[identity].ending_col = vert+2;
+        param[identity].ending_row = horz+2;
+        pthread_create(&(tid[identity]), NULL, board_piece, &param[identity]);
+        identity++;
+        if(vert == 6){
+            horz+=3;
+            vert = 0;
+        }
+        else{ 
+            vert+=3;
+        }
+    }
+    for(int i =0; i< 27; i++){
+        pthread_join(tid[i], NULL);
+    }
+
+    for (int v = 0; v < 27; v++){
+        if (valid[v] != 1){  
+            return 0; 
+        }
+    }
+    free(valid);
+    free(tid);
+   return 1;
 }
 
-
-int is_board_valid(){
+int is_board_valid()
     pthread_t* tid;  /* the thread identifiers */
     pthread_attr_t attr;
     param_struct* parameter;
@@ -87,7 +174,7 @@ int is_board_valid() {
     
 
 
- int validate_row(int** board, param_struct params) {
+ int validate_row(int** board, param_struct params) 
     int validate_arr[9];
     for(int i =params.starting_row; i<params.ending_row;i++) {
         for (int j = params.starting_col;i<params.ending_row;i++) {
@@ -103,7 +190,20 @@ int is_board_valid() {
         }
     }
     return 1;
- }
+ pthread_t* tid;  /* the thread identifiers */
+    pthread_attr_t attr;
+    param_struct* parameter;
+    tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);   
+    parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
+
+
+
+
+
+
+
+
+
 int validate_col() {
     
 }
